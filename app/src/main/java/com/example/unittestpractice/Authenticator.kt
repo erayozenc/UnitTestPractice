f48001d1f420da2ee5e5064cfc42e1318a8a124e
Eray <eryozenc@gmail.com>
2021-02-14 17:49:47 +0300

    test authentication

---
+++ app/src/main/java/com/example/unittestpractice/Authenticator.kt
@@ -0,0 +1,71 @@
+package com.example.unittestpractice
+
+import java.util.regex.Pattern
+
+object Authenticator {
+
+    private val userList = listOf<User>(
+            User("Brad","Pitt","bradpitt@hotmail.com","asd123")
+    )
+
+    fun register(
+            name : String,
+            surname : String,
+            email: String,
+            password: String,
+            confirmPassword: String
+    ) : Boolean {
+        if (name.isEmpty() || surname.isEmpty() || email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty())
+            return false
+        if (!email.contains("@") || !email.contains("."))
+            return false
+        if (!isValidPassword(password))
+            return false
+        if (password != confirmPassword)
+            return false
+        userList.map {
+            if (it.email == email)
+                return false
+        }
+        val newUser = User(name, surname, email, password)
+        return true
+
+    }
+
+    private fun isValidPassword(password: String) : Boolean {
+         val PASSWORD_PATTERN =
+                Pattern.compile("^" +
+                        "(?=.*[0-9])" +         //at least 1 digit
+                        "(?=.*[a-zA-Z])" +      //any letter
+                        "(?=\\S+$)" +           //no white spaces
+                        ".{6,}" +               //at least 4 characters
+                        "$")
+
+        return PASSWORD_PATTERN.matcher(password).matches()
+    }
+
+    fun loginWithEmailAndPassword(email : String, password: String) : Boolean {
+        if (email.isEmpty() || password.isEmpty())
+            return false
+        userList.map {
+            if (it.email == email){
+                return it.password == password //if true logged in, if false wrong password
+            }
+        }
+        return false //There is any account with this e-mail
+    }
+
+    fun forgetPassword(email: String) : String {
+        userList.map {
+            if (it.email == email)
+                return "TOKEN"
+        }
+        return "ERROR"
+    }
+
+    fun changePassword(token : String) : Boolean {
+        return token == "TOKEN"
+    }
+}
+
+data class User(val name: String,val surname: String,val email: String, val password: String)
---
+++ app/src/test/java/com/example/unittestpractice/AuthenticatorTest.kt
@@ -0,0 +1,108 @@
+package com.example.unittestpractice
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class AuthenticatorTest {
+
+    @Test
+    fun `if any input is empty when register, returns false`() {
+        val result1 = Authenticator.register("","Pitt","bradpitt@gmail.com","asd123","asd123")
+        val result2 = Authenticator.register("Brad","","bradpitt@gmail.com","asd123","asd123")
+        val result3 = Authenticator.register("Brad","Pitt","","asd123","asd123")
+        val result4 = Authenticator.register("Brad","Pitt","bradpitt@gmail.com","","asd123")
+        val result5 = Authenticator.register("Brad","Pitt","bradpitt@gmail.com","asd123","")
+        val totalResult = result1 || result2 || result3 || result4 || result5
+        assertThat(totalResult).isFalse()
+    }
+
+    @Test
+    fun `if password doesn't contain at least 6 characters and both number and letter, returns false`() {
+        val result = Authenticator.register("Brad","Pitt","bradpitt@gmail.com","123456","123456")
+        val result2 = Authenticator.register("Brad","Pitt","bradpitt@gmail.com","asdasd","asdasd")
+        val result3 = Authenticator.register("Brad","Pitt","bradpitt@gmail.com","asd12","asd12")
+        val totalResult = result || result2 || result3
+        assertThat(totalResult).isFalse()
+    }
+
+    @Test
+    fun `if password and confirm password is not same, returns false`() {
+        val result = Authenticator.register("Brad","Pitt","bradpitt@gmail.com","asd123","asd1234")
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `if e-mail is not proper when register, returns false`() {
+        val result = Authenticator.register("Brad","Pitt","bradpitt","asd123","asd123")
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `if e-mail is already taken, returns false`() {
+        // bradpitt@hotmail.com is already taken
+        val result = Authenticator.register("Brad","Pitt","bradpitt@hotmail.com","asd123","asd123")
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `if all parameters is proper when register, returns true`() {
+        val result = Authenticator.register("Brad","Pitt","bradpitt@gmail.com","asd123","asd123")
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun `if any input is empty when login, returns false`() {
+        val result1 = Authenticator.loginWithEmailAndPassword("","asd123")
+        val result2 = Authenticator.loginWithEmailAndPassword("bradpitt@hotmail.com","")
+        val totalResult = result1 || result2
+        assertThat(totalResult).isFalse()
+    }
+
+    @Test
+    fun `if e-mail is not proper when login, returns false`() {
+        val result = Authenticator.loginWithEmailAndPassword("bradpitt","asd123")
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `if e-mail and password is not true, returns false`() {
+        //proper account -> e-mail = bradpitt@hotmail.com password = asd123
+        val result = Authenticator.loginWithEmailAndPassword("bradpitt@hotmail.com","asd1234")
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `if e-mail and password is true, returns true`() {
+        //proper account -> e-mail = bradpitt@hotmail.com password = asd123
+        val result = Authenticator.loginWithEmailAndPassword("bradpitt@hotmail.com","asd123")
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun `if email is not proper or there is no account with this email when forget password, returns false`() {
+        val result = Authenticator.forgetPassword("bradpitt")
+        val result2 = Authenticator.forgetPassword("bradpitt@yahoo.com")
+        assertThat(result).isEqualTo("ERROR")
+        assertThat(result2).isEqualTo("ERROR")
+    }
+
+    @Test
+    fun `if email is proper when forget password, returns false`() {
+        val result = Authenticator.forgetPassword("bradpitt@hotmail.com")
+        assertThat(result).isEqualTo("TOKEN")
+    }
+
+    @Test
+    fun `if token is not proper when changing password, returns false`() {
+        //proper token is "TOKEN"
+        val result = Authenticator.changePassword("TOOOKEEEN")
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `if token is proper when changing password, returns false`() {
+        //proper token is "TOKEN"
+        val result = Authenticator.changePassword("TOKEN")
+        assertThat(result).isTrue()
+    }
+}
